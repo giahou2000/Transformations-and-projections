6d1ddb480e69d57836f8ec66bba13700dea23940
Christos <giachoud@gapps.auth.gr>
2023-05-17 21:46:11 +0300

    Rotation/Displacement

---
+++ RotationDisplacement.py
@@ -0,0 +1,36 @@
+from RotationMatrix import rotmat
+import numpy as np
+
+def RotateTranslate(cp, theta, u, A, t):
+
+    # Calculate the rotation matrix
+    R = rotmat(theta, u)
+    print("R: ")
+    print(R)
+    
+    # Translate the point cp to a coordinate system centered at point A
+    translated_cp = cp - A
+    print("translated_cp: ")
+    print(translated_cp)
+
+    # Rotate the translated point
+    temp = []
+    if len(translated_cp.shape) == 1:
+        rotated_cp = np.dot(R, translated_cp)
+    else:
+        for i in range(len(translated_cp.shape)):
+            temp.append(np.dot(R, translated_cp[i]))
+        rotated_cp = np.array(temp)
+    print("rotated_cp: ")
+    print(rotated_cp)
+    
+    # Translate the rotated point back to the original coordinate system
+    rotated_cp = rotated_cp + A
+    print("rotated_cp: ")
+    print(rotated_cp)
+
+    cq = rotated_cp + t
+    print("cq: ")
+    print(cq)
+
+    return cq
--- demo.py
+++ demo.py
@@ -1,11 +1,20 @@
 import numpy as np
 from RotationMatrix import rotmat
+from RotationDisplacement import RotateTranslate
 
-# Rotaion matrix calculation example
+# Extra details
 theta = np.pi  # 180 degrees/pi rads clockwise rotation
 u = np.array([1, 0, 0])  # Unit vector along the x-axis
+
+# Rotaion matrix calculation example
 rotation_mat = rotmat(theta, u)
 a = np.array([1, 2, 3])
-print(a)
-print(np.dot(rotation_mat, a))
+# print(a)
+# print(np.dot(rotation_mat, a))
 
+# Rotaion/Displacement calculation example
+cp = np.array([[1, 2, 3], [2, 4, 5]])
+# cp = np.array([1, 2, 3])
+A = np.array([1, 1, 4])
+t = np.array([3, 3, 3])
+cq = RotateTranslate(cp, theta, u, A, t)
