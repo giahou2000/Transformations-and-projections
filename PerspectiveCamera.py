409808a068c971b60077abb444831fc4a69686aa
Christos <giachoud@gapps.auth.gr>
2023-05-20 15:37:15 +0300

    Camera Looking At

--- PerspectiveCamera.py
+++ PerspectiveCamera.py
@@ -1,8 +1,41 @@
 import numpy as np
+from CoordinateSystems import ChangeCoordinateSystem
 
 def PinHole(f, cv, cx, cy, cz, p3d):
 
-    _, points_num = p3d.shape()
-    p2d = np.array((2, points_num))
-    p2d, depth = 0
+    # Find the rotation matrix for the change pf coordinates
+    rotation_matrix = np.column_stack((cx, cy, cz))
+
+    # Change coordinate system
+    new_p3d = ChangeCoordinateSystem(p3d, rotation_matrix, cv)
+
+    # Compute the projections
+    _, points_num = new_p3d.shape()
+    p2d = []
+    depth = []
+    for i in range(points_num):
+        p2d.append((f*new_p3d[i][0])/new_p3d[i][2], (f*new_p3d[i][1])/new_p3d[i][2])
+        depth.append(new_p3d[i][2])
+
+    return p2d, depth
+
+def CameraLookingAt(f, cv, cK, cup, p3d):
+
+    # cz computation
+    CK = cK - cv
+    CKnorm = np.linalg.norm(CK)
+    cz = CK/CKnorm
+
+    # cy computation
+    dot_product = np.dot(cup, cz)
+    cz_norm_squared = np.linalg.norm(cz) ** 2
+    projection = (dot_product / cz_norm_squared) * cz
+    t = cup - projection
+    cy = t / np.linalg.norm(t)
+
+    # cx computation
+    cx = np.outer(cy, cz)
+
+    p2d, depth = PinHole(f, cv, cx, cy, cz, p3d)
+
     return p2d, depth
--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import numpy as np
 from RotationMatrix import rotmat
 from RotationDisplacement import RotateTranslate
 from CoordinateSystems import ChangeCoordinateSystem
+from PerspectiveCamera import PinHole, CameraLookingAt
 
 # Extra details
 theta = np.pi  # 180 degrees/pi rads clockwise rotation
@@ -26,3 +27,17 @@ c0 = np.array([0, 0, 0])
 cq = ChangeCoordinateSystem(cp, R, c0)
 print("cq:")
 print(cq)
+
+# 4) Perspective pinhole camera example
+f = 5
+cv = 3
+cx = np.array([1, 2, 7])
+cy = np.array([1, 1, 5])
+cz = np.array([1, 5, 4])
+p3d = np.array([[1, 2, 3], [2, 4, 5]])
+p2d, depth = PinHole(f, cv, cx, cy, cz, p3d)
+
+# 5) Perspective pinhole camera example 2
+cK = 0
+cup = 0
+p2d, depth = CameraLookingAt(f, cv, cK, cup, p3d)
