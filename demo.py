37ab3bb7961ca17d5f6a9bc19697de9e6717d8c5
Christos <giachoud@gapps.auth.gr>
2023-05-25 14:25:50 +0300

    Well whatever

--- demo.py
+++ demo.py
@@ -24,16 +24,27 @@ f = 70
 p3d = np.transpose(p3d)
 I = RenderObject(p3d, faces, vcolors, H, W, Rows, Columns, f, cv, cK, cup)
 matplotlib.pyplot.imshow(I)
-# matplotlib.pyplot.savefig('I_0_1.png')
+# matplotlib.pyplot.savefig('I_1.png')
 matplotlib.pyplot.show()
 
-# # Print the second version of the image
+# Print the second version of the image
 disp_p3d = p3d + t1
-# I = RenderObject(disp_p3d, faces, vcolors, H, W, Rows, Columns, f, cv, cK, cup)
-# matplotlib.pyplot.imshow(I)
-# # matplotlib.pyplot.savefig('flat.png')
-# matplotlib.pyplot.show()
+I = RenderObject(disp_p3d, faces, vcolors, H, W, Rows, Columns, f, cv, cK, cup)
+matplotlib.pyplot.imshow(I)
+# # matplotlib.pyplot.savefig('I_2.png')
+matplotlib.pyplot.show()
 
-# # Print the third version of the image
-# A = np.array([0, 0, 0])
-# disp_p3d = RotateTranslate(p3d, phi, u, A, t1)
+# Print the third version of the image
+A = np.array([0, 0, 0])
+disp_p3d = RotateTranslate(disp_p3d, phi, u, A, np.array([0, 0, 0]))
+I = RenderObject(disp_p3d, faces, vcolors, H, W, Rows, Columns, f, cv, cK, cup)
+matplotlib.pyplot.imshow(I)
+# matplotlib.pyplot.savefig('I_3.png')
+matplotlib.pyplot.show()
+
+# Print the fourth version of the image
+disp_p3d = disp_p3d + t2
+I = RenderObject(disp_p3d, faces, vcolors, H, W, Rows, Columns, f, cv, cK, cup)
+matplotlib.pyplot.imshow(I)
+# # matplotlib.pyplot.savefig('I_4.png')
+matplotlib.pyplot.show()
--- photography.py
+++ photography.py
@@ -21,9 +21,9 @@ def RenderObject(p3d, faces, vcolors, H, W, Rows, Columns, f, cv, cK, cup):
 
     # Compute 2D projection and depth of the image
     p2d, depth = CameraLookingAt(f, cv , cK , cup , p3d)
-    # for element in p2d:
-    #     if element[0] < -7.5 or element[1] < -7.5 or element[0] > 7.5 or element[1] > 7.5:
-    #         print(element)
+    for element in p2d:
+        if element[0] < -7.5 or element[1] < -7.5 or element[0] > 7.5 or element[1] > 7.5:
+            print(element)
 
     # Rasterize the 2D image
     n2d = rasterize(p2d, Rows, Columns, H, W)
